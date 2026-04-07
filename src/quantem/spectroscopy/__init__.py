b23d2550e913dac43f099e2497850e9d1a438d34
yaeltsarfati <your-new-email@example.com>
2026-04-07 11:59:58 -0700

    Update spectroscopy init by removing imports of deleted modules.

--- src/quantem/spectroscopy/__init__.py
+++ src/quantem/spectroscopy/__init__.py
@@ -8,7 +8,3 @@ from quantem.spectroscopy.dataset3deels import (
 from quantem.spectroscopy.dataset3deds import (
     Dataset3deds as Dataset3deds,
 )
-
-from quantem.spectroscopy import thickness as thickness
-from quantem.spectroscopy import eels_alignment as eels_alignment
-from quantem.spectroscopy import eels_visualization as eels_visualization
